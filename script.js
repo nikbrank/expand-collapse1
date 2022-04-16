81923f02b514bb186039776f078c4b6cf92a7903
nikbrank <brankovicnikola10@gmail.com>
2022-04-16 20:38:49 +0200

    commit

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ button.forEach((button) => {
     }
   });
 });
+
+// document.addEventListener("click", (e) => {
+//   if (!e.target.matches(".expand-button")) return;
+
+//   const card = e.target.closest(".card");
+//   const cardBody = card.querySelector(".card-body");
+
+//   cardBody.classList.toggle("show");
+//   e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand"; // Ako se innerText slaze sa expand, napisi collapse, ako jeste expand ostavi expand
+// });
